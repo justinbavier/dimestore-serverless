49d706cda9caf46102810007a14ec357b141512b
Ben Miner <ben.miner@pop.belmont.edu>
2018-10-09 13:37:44 -0500

    Added local AWS variables to npm start command to avoid using other AWS accounts

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const AWS_REGION = process.env.AWS_REGION || 'us-east-1';
-export const DYNAMO_ACCESS_KEY_ID = process.env.DIME_AWS_ACCESS_KEY_ID;
+export const DYNAMO_ACCESS_KEY_ID = process.env.DIME_AWS_ACCESS_KEY;
 export const DYNAMO_SECRET_ACCESS_KEY = process.env.DIME_AWS_SECRET_KEY;
 export const STRIPE_SECRET = process.env.DIME_STRIPE_SECRET;
 export const PLAID_CLIENT_ID = process.env.DIME_PLAID_CLIENT_ID;
--- src/routes/getCharityById.ts
+++ src/routes/getCharityById.ts
@@ -6,7 +6,6 @@ import { path } from 'ramda';
 
 export default cors((event, _context, callback) => {
     const { id } = event.pathParameters;
-
     return DimeCharity.get(id)
         .then(charity => callback(null, ok({ success: true, charity: path(['attrs'], charity) })))
     .catch(error => callback(null, badRequest({ message: `Bad Request -> ${error}` })))
