f44ebd954e61f0bc1f33f1081a073490748fa1a5
Ben Miner <ben.miner@pop.belmont.edu>
2019-01-06 14:51:18 -0600

    added first name and last name to create user

--- src/routes/createUser.ts
+++ src/routes/createUser.ts
@@ -2,14 +2,16 @@ import { DimeUser } from '../models';
 import cors from '../util/cors';
 import ok from '../util/ok';
 import badRequest from '../util/badRequest';
-import { path } from 'ramda';
+import { path, prop } from 'ramda';
 
 export default cors((event, _context, callback) => {
     const body = JSON.parse(event.body);
 
     const user = {
-        id: body.cognitoId,
-        email: body.email,
+        id: prop('cognitoId', body),
+        email: prop('email', body),
+        firstName: prop('firstName', body),
+        lastName: prop('lastName', body),
         progress: 0,
         totalDonated: 0,
         donationThreshold: 1000,
